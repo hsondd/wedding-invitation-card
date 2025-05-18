626d938e03373f3008c826fb00a4996248ba4f1b
hsondd <sondhust@gmail.com>
2025-05-18 18:11:40 +0700

    Update wedding date in HTML and JavaScript files to June 29, 2025

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 9, 2025 00:00:00").getTime();
+var countDownDate = new Date("Jun 29, 2025 11:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {

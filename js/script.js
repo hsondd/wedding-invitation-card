f0582926e7359d7eb399ad078dff239bf5c4c17b
hsondd <sondhust@gmail.com>
2025-05-18 20:18:53 +0700

    kjj

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 29, 2025 11:00:00").getTime();
+var countDownDate = new Date("Jun 29, 2025 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
